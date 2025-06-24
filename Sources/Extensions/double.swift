184b8d8fdd4d1fc65910b97dcf1c788e68fd0418
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-24 23:39:00 +0200

    modified ext

--- Sources/Extensions/double.swift
+++ Sources/Extensions/double.swift
@@ -2,6 +2,29 @@ import Foundation
 import plate
 import Structures
 
+public enum AccountBalancePolarity: Sendable {
+    case payable
+    case receivable
+
+    public func overviewLabel() -> String {
+        switch self {
+            case .payable: 
+                return "Te betalen"
+            case .receivable: 
+                return "Te ontvangen"
+        }
+    }
+
+    public func documentLabel() -> String {
+        switch self {
+            case .payable: 
+                return "Factuur"
+            case .receivable: 
+                return "Terugbetaling"
+        }
+    }
+}
+
 extension Double {
     public func orient(to direction: ValueDirection) -> Double {
         switch direction {
@@ -12,7 +35,7 @@ extension Double {
         }
     }
 
-    public var payableOrReceivableLabel: String {
-        return self >= 0 ? "Te betalen" : "Te ontvangen"
+    public func payableOrReceivable() -> AccountBalancePolarity {
+        return self >= 0 ? .payable : .receivable
     }
 }
