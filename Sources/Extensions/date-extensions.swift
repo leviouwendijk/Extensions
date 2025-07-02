7208ea73cee738c724343a8f6926c2f60cb5a377
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-02 12:35:52 +0200

    adding weeks to resolve breaking change in structures

--- Sources/Extensions/date-extensions.swift
+++ Sources/Extensions/date-extensions.swift
@@ -17,35 +17,38 @@ extension Date: DateDistanceCalculable {
         let calendar = Calendar.current
         
         switch unit {
-        case .days:
-            return calendar.dateComponents([.day], from: self, to: date)
-            
-        case .months:
-            return calendar.dateComponents([.month], from: self, to: date)
-            
-        case .years:
-            return calendar.dateComponents([.year], from: self, to: date)
-            
-        case .hours:
-            return calendar.dateComponents([.hour], from: self, to: date)
-            
-        case .minutes:
-            return calendar.dateComponents([.minute], from: self, to: date)
-            
-        case .seconds:
-            return calendar.dateComponents([.second], from: self, to: date)
-            
-        case .milliseconds:
-            let interval = date.timeIntervalSince(self)
-            let millis = Int(interval * 1_000)
-            return DateComponents(nanosecond: millis * 1_000_000)
-            
-        case .combined:
-            return calendar.dateComponents(
-                [.year, .month, .day, .hour, .minute, .second],
-                from: self,
-                to: date
-            )
+            case .days:
+                return calendar.dateComponents([.day], from: self, to: date)
+
+            case .weeks:
+                return calendar.dateComponents([.weekOfYear], from: self, to: date)
+                
+            case .months:
+                return calendar.dateComponents([.month], from: self, to: date)
+                
+            case .years:
+                return calendar.dateComponents([.year], from: self, to: date)
+                
+            case .hours:
+                return calendar.dateComponents([.hour], from: self, to: date)
+                
+            case .minutes:
+                return calendar.dateComponents([.minute], from: self, to: date)
+                
+            case .seconds:
+                return calendar.dateComponents([.second], from: self, to: date)
+                
+            case .milliseconds:
+                let interval = date.timeIntervalSince(self)
+                let millis = Int(interval * 1_000)
+                return DateComponents(nanosecond: millis * 1_000_000)
+                
+            case .combined:
+                return calendar.dateComponents(
+                    [.year, .month, .day, .hour, .minute, .second],
+                    from: self,
+                    to: date
+                )
         }
     }
 }
@@ -96,32 +99,41 @@ public func timeDistance(
     
     let raw: Int
     switch unit {
-    case .days:
-        raw = try singleComponent(.day,    for: .days)
-    case .months:
-        raw = try singleComponent(.month,  for: .months)
-    case .years:
-        raw = try singleComponent(.year,   for: .years)
-    case .hours:
-        raw = try singleComponent(.hour,   for: .hours)
-    case .minutes:
-        raw = try singleComponent(.minute, for: .minutes)
-    case .seconds:
-        raw = try singleComponent(.second, for: .seconds)
-        
-    case .milliseconds:
-        let interval = end.timeIntervalSince(start)
-        raw = Int(interval * 1_000)
-        
-    case .combined:
-        throw TimeDistanceError.invalidUnit
+        case .milliseconds:
+            let interval = end.timeIntervalSince(start)
+            raw = Int(interval * 1_000)
+
+        case .seconds:
+            raw = try singleComponent(.second, for: .seconds)
+
+        case .minutes:
+            raw = try singleComponent(.minute, for: .minutes)
+
+        case .hours:
+            raw = try singleComponent(.hour,   for: .hours)
+
+        case .days:
+            raw = try singleComponent(.day,    for: .days)
+
+        case .weeks:
+            raw = try singleComponent(.weekOfYear, for: .weeks)
+
+        case .months:
+            raw = try singleComponent(.month,  for: .months)
+
+        case .years:
+            raw = try singleComponent(.year,   for: .years)
+
+            
+        case .combined:
+            throw TimeDistanceError.invalidUnit
     }
 
     switch method {
-    case .until:
-        return raw
-    case .through:
-        return raw + 1
+        case .until:
+            return raw
+        case .through:
+            return raw + 1
     }
 }
 
