7d47ff9ceff6014f407c9ba6bc14a3ef8968c338
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-14 15:23:40 +0200

    ext

--- Sources/Extensions/string-extensions.swift
+++ Sources/Extensions/string-extensions.swift
@@ -22,4 +22,33 @@ extension String {
             options: .regularExpression
         )
     }
+
+    public func emDashedFromHyphens() -> String {
+        return self.replacingOccurrences(of: "--", with: "—")
+    }
+
+    // public func contexts(of substring: String) -> [(before: Character?, after: Character?)] {
+    //     var results: [(Character?, Character?)] = []
+    //     guard !substring.isEmpty else { return results }
+
+    //     let full = Array(self)
+    //     let pattern = NSRegularExpression.escapedPattern(for: substring)
+    //     let re = try! NSRegularExpression(pattern: pattern)
+
+    //     let ns = self as NSString
+    //     let ranges = re.matches(in: self, range: NSRange(location: 0, length: ns.length))
+    //     .map { $0.range }
+
+    //     for range in ranges {
+    //         let start = range.location
+    //         let end = range.location + range.length - 1
+
+    //         let before: Character? = (start > 0) ? full[start - 1] : nil
+
+    //         let after: Character? = (end + 1 < full.count) ? full[end + 1] : nil
+
+    //         results.append((before, after))
+    //     }
+    //     return results
+    // }
 }
