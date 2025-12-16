095eaf44e94235a56291f37dc56f36ff1674805c
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-12-16 19:52:12 +0100

    removal of depr ref

--- Sources/Extensions/Economics/expiration-extensions.swift
+++ Sources/Extensions/Economics/expiration-extensions.swift
@@ -1,7 +1,7 @@
-import Structures
+// import Structures
 
-extension ExpirationDateRange {
-    public func string(in format: String = "dd/MM/yyyy") -> String {
-        return "\(start.conforming(to: format)) - \(end.conforming(to: format))"
-    }
-}
+// extension ExpirationDateRange {
+//     public func string(in format: String = "dd/MM/yyyy") -> String {
+//         return "\(start.conforming(to: format)) - \(end.conforming(to: format))"
+//     }
+// }
